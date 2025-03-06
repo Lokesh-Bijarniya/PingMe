92d175048ad26a24e0b6ec83245c4607dd8d1c03
Lokesh_Bijarniya_ <152880035+Lokesh-Bijarniya@users.noreply.github.com>
2025-03-06 20:18:38 +0530

    updt google auth

--- v1/server/src/config/googleAuthController.js
+++ v1/server/src/config/googleAuthController.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.CLIENT_URL}/v1/api/auth/google/callback`,  // Ensure this matches your redirect URI in Google Cloud Console
+      callbackURL: `https://pingme-wkue.onrender.com/v1/api/auth/google/callback`,  
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
