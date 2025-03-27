693ebab9cddbe7fd853f2f5d4e0970acf2998c46
Lokesh_Bijarniya_ <152880035+Lokesh-Bijarniya@users.noreply.github.com>
2025-03-27 23:04:18 +0530

    updt redis config

--- v1/server/src/config/redis.js
+++ v1/server/src/config/redis.js
@@ -1,6 +1,9 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = new createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
 redisClient.connect()
   .then(() => console.log("🔥 Redis Connected"))
   .catch((err) => console.error("❌ Redis Connection Failed:", err));
